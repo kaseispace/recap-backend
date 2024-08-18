99adc241ef279c0ac2cc1c5beb2f2f018195e0f4
yuki0829 <fledgling0829@gmail.com>
2024-08-18 21:19:16 +0900

    feat: 学校データ取得APIの作成

---
+++ app/controllers/api/v1/schools_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class SchoolsController < ApplicationController
+      # skip_before_action :authenticate, only: [:index, :show]
+
+      # 学校一覧
+      def index
+        schools = School.select(:id, :name).order(:id)
+        render json: schools
+      end
+
+      # 特定の学校
+      def show
+        schools = School.find(params[:id])
+        render json: schools
+      end
+    end
+  end
+end
