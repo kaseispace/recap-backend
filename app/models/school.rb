f9cad4163a4105be5936faee1a55a7b9940329e5
yuki0829 <fledgling0829@gmail.com>
2024-08-19 15:22:37 +0900

    feat: Rails/HasManyOrHasOneDependentルールを無効化

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,7 @@
 class School < ApplicationRecord
+  # rubocop:disable Rails/HasManyOrHasOneDependent
   has_many :user_schools
+  # rubocop:enable Rails/HasManyOrHasOneDependent
   has_many :users, through: :user_school
 
   validates :name, presence: true
