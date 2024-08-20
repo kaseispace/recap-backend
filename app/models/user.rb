26d2cb6a7c909edca8a932f06c2773151eb3317c
yuki0829 <fledgling0829@gmail.com>
2024-08-20 10:27:35 +0900

    feat: courseテーブルとの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_schools, dependent: :destroy
   has_many :schools, through: :user_schools
+  has_many :courses, dependent: :destroy
 
   validates :uid, presence: true, uniqueness: true
   validates :name, presence: true
