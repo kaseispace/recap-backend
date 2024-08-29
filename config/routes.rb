f3bfd0fc50bcaa04d0d40399300d7011e9022508
yuki0829 <fledgling0829@gmail.com>
2024-08-29 10:33:29 +0900

    refactor: coursesリソースのルーティングをUUIDベースに変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :destroy], param: :uid
       resources :user_schools, only: [:index, :create]
 
-      resources :courses, only: [:index, :create, :update, :destroy] do
+      resources :courses, only: [:index, :create, :update, :destroy], param: :uuid do
         member do
           get 'joined_users'
         end
