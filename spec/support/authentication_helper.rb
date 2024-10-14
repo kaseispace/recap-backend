e6f3ff86070f610d53fca65913f640a7dc2d9a3f
yuki0829 <fledgling0829@gmail.com>
2024-10-14 21:46:41 +0900

    feat: 認証ヘルパーにUserSchoolsControllerを追加

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -5,6 +5,7 @@ module AuthenticationHelper
     before do
       allow_any_instance_of(ApplicationController).to receive(:authenticate)
       allow_any_instance_of(Api::V1::UsersController).to receive(:payload_uid).and_return('mock_uid')
+      allow_any_instance_of(Api::V1::UserSchoolsController).to receive(:payload_uid).and_return('mock_uid')
     end
   end
 end
