4d02e830123d5ec5782e65bfe20f4bd8f9b45914
yuki0829 <fledgling0829@gmail.com>
2024-10-17 20:37:04 +0900

    refactor: set_userメソッドから@payloadを分離

--- app/controllers/api/v1/user_courses_controller.rb
+++ app/controllers/api/v1/user_courses_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class UserCoursesController < ApplicationController
+      before_action :payload_uid, only: %i[index show create destroy]
       before_action :set_user, only: %i[index show create destroy]
 
       def index
@@ -15,44 +16,43 @@ module Api
 
       def create
         course = Course.find_by(school_id: params[:school_id], course_code: params[:course_code])
-        if course.nil?
-          render json: { error: { messages: ['コースが見つかりませんでした。'] } }, status: :not_found
+        return render json: { error: { messages: ['授業が存在しませんでした。'] } }, status: :not_found unless course
+
+        user_course = UserCourse.find_or_initialize_by(user_id: @user.id, course_id: course.id)
+        return render json: { error: { messages: ['既に参加済みです。'] } }, status: :conflict unless user_course.new_record?
+
+        if user_course.save
+          render json: course.as_json(only: %i[name teacher_name day_of_week course_time uuid])
         else
-          user_course = UserCourse.find_or_initialize_by(user_id: @user.id, course_id: course.id)
-          if user_course.new_record?
-            if user_course.save
-              render json: course.as_json(only: %i[name teacher_name day_of_week course_time uuid])
-            else
-              render json: { error: { messages: ['コースに参加できませんでした。'] } }, status: :unprocessable_entity
-            end
-          else
-            render json: { error: { messages: ['登録済みです。'] } }, status: :conflict
-          end
+          render json: { error: { messages: ['授業に参加できませんでした。'] } }, status: :unprocessable_entity
         end
       end
 
       def destroy
         course = Course.find_by(uuid: params[:uuid])
-        if course
-          user_course = UserCourse.find_by(course_id: course.id, user_id: @user.id)
-          if user_course
-            if user_course.destroy
-              head :no_content
-            else
-              render json: { error: { messages: ['コースから退出できませんでした。'] } }, status: :unprocessable_entity
-            end
-          else
-            render json: { error: { messages: ['あなたはこのコースの参加者ではないため、退出できません。'] } }, status: :forbidden
-          end
+        return render json: { error: { messages: ['授業が存在しませんでした。'] } }, status: :not_found unless course
+
+        user_course = UserCourse.find_by(course_id: course.id, user_id: @user.id)
+        unless user_course
+          return render json: { error: { messages: ['あなたはこの授業の参加者ではないため、退出できません。'] } },
+                        status: :forbidden
+        end
+
+        if user_course.destroy
+          head :no_content
         else
-          render json: { error: { messages: ['該当するコースが存在しませんでした'] } }, status: :not_found
+          render json: { error: { messages: ['授業から退出できませんでした。'] } }, status: :unprocessable_entity
         end
       end
 
       private
 
+      def payload_uid
+        @payload['user_id']
+      end
+
       def set_user
-        @user = User.find_by(uid: @payload['user_id'])
+        @user = User.find_by(uid: payload_uid)
       end
     end
   end
