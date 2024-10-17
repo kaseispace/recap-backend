abc6f7bcfd6926eac65133fc09c1e5b2012d1dcd
yuki0829 <fledgling0829@gmail.com>
2024-10-17 10:37:06 +0900

    feat: 認証ヘルパーにAnnouncementsControllerを追加

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -7,6 +7,14 @@ module AuthenticationHelper
       allow_any_instance_of(Api::V1::UsersController).to receive(:payload_uid).and_return('mock_uid')
       allow_any_instance_of(Api::V1::UserSchoolsController).to receive(:payload_uid).and_return('mock_uid')
       allow_any_instance_of(Api::V1::CoursesController).to receive(:payload_uid).and_return('mock_uid')
+      allow_any_instance_of(Api::V1::AnnouncementsController).to receive(:payload_uid) do |controller|
+        case controller.params[:scenario]
+        when 'student'
+          'mock_student_uid'
+        else
+          'mock_uid'
+        end
+      end
     end
   end
 end
