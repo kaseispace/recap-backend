10c2889e20e715c1c3c9d4cba59da54b334b77cc
yuki0829 <fledgling0829@gmail.com>
2024-10-17 11:56:26 +0900

    refactor: テストケース名を分かりやすく変更

--- spec/controllers/api/v1/schools_spec.rb
+++ spec/controllers/api/v1/schools_spec.rb
@@ -5,21 +5,28 @@ RSpec.describe Api::V1::SchoolsController, type: :controller do
   let!(:school) { FactoryBot.create(:school) }
 
   describe 'GET /api/v1/schools' do
-    it '学校一覧の取得に成功する' do
+    it '学校一覧の取得に成功する（ステータスコード200）' do
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /api/v1/schools/:id' do
-    it '指定した学校の取得に成功する' do
-      get :show, params: { id: school.id }
+    before do
+      @valid_params = { id: school.id }
+      @invalid_params = { id: 9999 }
+    end
+
+    it '指定した学校の取得に成功する（ステータスコード200）' do
+      get :show, params: @valid_params
       expect(response).to have_http_status(:success)
     end
 
-    it '指定した学校が存在しない' do
-      get :show, params: { id: 9999 }
-      expect(response).to have_http_status(:not_found)
+    it '一致する学校がない場合、空のオブジェクトを返す（ステータスコード200）' do
+      get :show, params: @invalid_params
+      expect(response).to have_http_status(:success)
+      json_response = response.parsed_body
+      expect(json_response).to be_nil
     end
   end
 end
--- spec/controllers/api/v1/users_spec.rb
+++ spec/controllers/api/v1/users_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
+  let!(:user) { FactoryBot.create(:user) }
+
   describe 'POST /api/v1/users' do
     include AuthenticationHelper
 
@@ -10,20 +12,16 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
 
     context '有効なパラメータ' do
-      it '新しいユーザーを作成する' do
+      it 'ユーザーの作成に成功する（ステータスコード200）' do
         expect do
           post :create, params: @valid_params
         end.to change(User, :count).by(1)
-      end
-
-      it 'ステータスコード200が返される' do
-        post :create, params: @valid_params
         expect(response).to have_http_status(:success)
       end
     end
 
     context '無効なパラメータ' do
-      it 'ステータスコード422が返される' do
+      it 'ユーザーの作成に失敗する（ステータスコード422）' do
         post :create, params: @invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
@@ -31,35 +29,34 @@ RSpec.describe Api::V1::UsersController, type: :controller do
   end
 
   describe 'DELETE /api/v1/users' do
-    let!(:user) { FactoryBot.create(:user) }
+    before do
+      @valid_params = { uid: user.uid }
+      @invalid_params = { uid: 'nonexistent_uid_9999' }
+    end
 
     context 'ユーザーが認証されている場合' do
       include AuthenticationHelper
 
-      context '有効なUID' do
-        it 'ユーザーを削除する' do
+      context '有効なパラメータ' do
+        it 'ユーザーの削除に成功する（ステータスコード200）' do
           expect do
-            delete :destroy, params: { uid: user.uid }
+            delete :destroy, params: @valid_params
           end.to change(User, :count).by(-1)
-        end
-
-        it 'ステータスコード204が返される' do
-          delete :destroy, params: { uid: user.uid }
           expect(response).to have_http_status(:no_content)
         end
       end
 
-      context '無効なUID' do
-        it 'ステータスコード404が返される' do
-          delete :destroy, params: { uid: 'nonexistent_uid_12345' }
+      context '無効なパラメータ' do
+        it '一致するユーザーがない場合、削除に失敗する（ステータスコード404）' do
+          delete :destroy, params: @invalid_params
           expect(response).to have_http_status(:not_found)
         end
       end
     end
 
     context 'ユーザーが認証されていない場合' do
-      it 'ステータスコード401が返される' do
-        delete :destroy, params: { uid: user.uid }
+      it 'ユーザーの削除に失敗する（ステータスコード401）' do
+        delete :destroy, params: @valid_params
         expect(response).to have_http_status(:unauthorized)
       end
     end
