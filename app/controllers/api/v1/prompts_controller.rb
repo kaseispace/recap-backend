8aaad2db9bea2ca830911d7d17336b960ab34fb4
yuki0829 <fledgling0829@gmail.com>
2024-10-18 22:16:01 +0900

    refactor: コントローラのリファクタリング

--- app/controllers/api/v1/prompts_controller.rb
+++ app/controllers/api/v1/prompts_controller.rb
@@ -1,123 +1,114 @@
 module Api
   module V1
     class PromptsController < ApplicationController
+      before_action :payload_uid, only: %i[teacher_prompts student_prompt activate_prompt create update destroy]
       before_action :set_user,
                     only: %i[teacher_prompts student_prompt activate_prompt create update destroy]
 
       # 授業内で作成されたプロンプトを取得
       def teacher_prompts
         course = Course.find_by(created_by_id: @user.id, uuid: params[:uuid])
-        if course
-          prompts = Prompt.includes(:prompt_questions).where(course_id: course.id)
-          render json: prompts, include: :prompt_questions
-        else
-          render json: { error: { messages: ['あなたが作成した振り返りが見つかりませんでした。コースIDを確認してください。'] } },
-                 status: :not_found
+        unless course
+          return render json: { error: { messages: ['あなたが作成したコースが見つかりませんでした。コースIDを確認してください。'] } },
+                        status: :not_found
         end
+
+        prompts = Prompt.includes(:prompt_questions).where(course_id: course.id)
+        render json: prompts, include: :prompt_questions
       end
 
       # 学生に公開するプロンプトを取得
       def student_prompt
         user_course = UserCourse.joins(:course).find_by(user_id: @user.id, 'courses.uuid' => params[:uuid])
-        if user_course
-          prompt = Prompt.includes(:prompt_questions).find_by(course_id: user_course.course_id, active: true)
-          if prompt
-            contents = prompt.prompt_questions.map(&:content)
-            render json: contents
-          else
-            render json: []
-          end
+        unless user_course
+          return render json: { error: { messages: ['あなたが所属するコースが見つかりませんでした。コースIDを確認してください。'] } },
+                        status: :not_found
+        end
+
+        prompt = Prompt.includes(:prompt_questions).find_by(course_id: user_course.course_id, active: true)
+        if prompt
+          contents = prompt.prompt_questions.map(&:content)
+          render json: contents
         else
-          render json: { error: { messages: ['あなたが所属するコースが見つかりませんでした。コースIDを確認してください。'] } }, status: :not_found
+          render json: []
         end
       end
 
       # プロンプトをactiveにする
       def activate_prompt
-        prompt = Prompt.find_by(id: params[:id])
-        if prompt
-          if Course.find_by(id: prompt.course_id, created_by_id: @user.id)
-            Prompt.activate(params[:id])
-            head :no_content
-          else
-            render json: { error: { messages: ['あなたは授業の作成者ではないので振り返りを編集できません'] } }, status: :forbidden
-          end
+        prompt = Prompt.includes(:course).find_by(id: params[:id])
+        return render json: { error: { messages: ['プロンプトが存在しませんでした。'] } }, status: :not_found unless prompt
+
+        unless prompt.course.created_by_id == @user.id
+          return render json: { error: { messages: ['あなたは授業の作成者ではないのでプロンプトの有効化はできません。'] } },
+                        status: :forbidden
+        end
+
+        if Prompt.activate(params[:id])
+          head :no_content
         else
-          render json: { error: { messages: ['振り返りが存在しませんでした。'] } }, status: :not_found
+          render json: { error: { messages: ['プロンプトの有効化に失敗しました。'] } }, status: :unprocessable_entity
         end
       end
 
       def create
         course = Course.find_by(uuid: params[:uuid], created_by_id: @user.id)
-        if course
-          prompt = Prompt.new(prompt_params)
-          prompt.course_id = course.id
-          if prompt.save
-            params[:contents].each do |content|
-              PromptQuestion.create(prompt_id: prompt.id, content:)
-            end
-            prompt_questions = prompt.prompt_questions
-            render json: prompt.attributes.merge(prompt_questions:)
-          else
-            render json: { error: { messages: ['振り返りを登録できませんでした。'] } }, status: :unprocessable_entity
-          end
+        return render json: { error: { messages: ['あなたは授業の作成者ではないのでプロンプトの作成はできません。'] } }, status: :forbidden unless course
+
+        prompt = Prompt.new(prompt_params)
+        prompt.course = course
+
+        if prompt.save
+          render json: prompt
         else
-          render json: { error: { messages: ['あなたは授業の作成者ではないので振り返りを投稿できません'] } }, status: :forbidden
+          render json: { error: { messages: ['プロンプトを登録できませんでした。'] } }, status: :unprocessable_entity
         end
       end
 
       def update
-        prompt = Prompt.find_by(id: params[:id])
-        if prompt
-          if Course.find_by(id: prompt.course_id, created_by_id: @user.id)
-            ActiveRecord::Base.transaction do
-              if prompt.update(prompt_params)
-                prompt.prompt_questions.destroy_all
-                params[:contents].each do |content|
-                  PromptQuestion.create(prompt_id: prompt.id, content:)
-                end
-                prompt.reload
-                prompt_questions = prompt.prompt_questions.includes(:prompt)
-                render json: prompt.attributes.merge(prompt_questions:)
-              else
-                render json: { error: { messages: ['振り返りの更新に失敗しました。'] } }, status: :unprocessable_entity
-              end
-            end
-          else
-            render json: { error: { messages: ['あなたは授業の作成者ではないので振り返りを編集できません'] } }, status: :forbidden
-          end
+        prompt = Prompt.includes(:course).find_by(id: params[:id])
+        return render json: { error: { messages: ['プロンプトが存在しませんでした。'] } }, status: :not_found unless prompt
+
+        unless prompt.course.created_by_id == @user.id
+          return render json: { error: { messages: ['あなたは授業の作成者ではないのでプロンプトを編集できません。'] } },
+                        status: :forbidden
+        end
+
+        if prompt.update(prompt_params)
+          render json: @prompt
         else
-          render json: { error: { messages: ['振り返りが存在しませんでした。'] } }, status: :not_found
+          render json: { error: { messages: ['プロンプトの編集に失敗しました。'] } }, status: :unprocessable_entity
         end
       end
 
       def destroy
-        prompt = Prompt.find_by(id: params[:id])
-        if prompt
-          course = Course.find_by(id: prompt.course_id, created_by_id: @user.id)
-          if course
-            if prompt.destroy
-              head :no_content
-            else
-              render json: { error: { messages: ['振り返りの削除に失敗しました。'] } }, status: :unprocessable_entity
-            end
-          else
-            render json: { error: { messages: ['コースの作成者でないため、振り返りを削除できません。'] } }, status: :forbidden
-          end
+        prompt = Prompt.includes(:course).find_by(id: params[:id])
+        return render json: { error: { messages: ['プロンプトが存在しませんでした。'] } }, status: :not_found unless prompt
+
+        unless prompt.course.created_by_id == @user.id
+          return render json: { error: { messages: ['あなたは授業の作成者ではないのでプロンプトを削除できません。'] } },
+                        status: :forbidden
+        end
+
+        if prompt.destroy
+          head :no_content
         else
-          render json: { error: { messages: ['振り返りが存在しませんでした。'] } }, status: :not_found
+          render json: { error: { messages: ['プロンプトの削除に失敗しました。'] } }, status: :unprocessable_entity
         end
       end
 
       private
 
-      # ユーザーを探すメソッド
+      def payload_uid
+        @payload['user_id']
+      end
+
       def set_user
-        @user = User.find_by(uid: @payload['user_id'])
+        @user = User.find_by(uid: payload_uid)
       end
 
       def prompt_params
-        params.require(:prompt).permit(:title)
+        params.require(:prompt).permit(:title, prompt_questions_attributes: %i[id content _destroy])
       end
     end
   end
