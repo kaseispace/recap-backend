c066939980b29cf6fc4d585bd880a6bec67a5651
yuki0829 <fledgling0829@gmail.com>
2024-10-18 22:14:58 +0900

    feat: Promptモデルにネストされた属性とカスタムバリデーションを追加

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,8 +1,17 @@
 class Prompt < ApplicationRecord
   belongs_to :course
   has_many :prompt_questions, dependent: :destroy
+  accepts_nested_attributes_for :prompt_questions, allow_destroy: true
 
   validates :title, presence: true
+  validates_associated :prompt_questions
+  validate :must_have_at_least_one_prompt_question
+
+  def must_have_at_least_one_prompt_question
+    return unless prompt_questions.empty?
+
+    errors.add(:prompt_questions, '少なくとも1つの質問を含めてください。')
+  end
 
   def self.activate(prompt_id)
     prompt = find(prompt_id)
@@ -12,5 +21,12 @@ class Prompt < ApplicationRecord
       end
       prompt.update!(active: true)
     end
+    true
+  rescue StandardError
+    false
+  end
+
+  def as_json(options = {})
+    super(options.merge(include: :prompt_questions))
   end
 end
