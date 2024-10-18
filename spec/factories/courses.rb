134a9b836eef0d2c6c080403ab78c1bbd030111a
yuki0829 <fledgling0829@gmail.com>
2024-10-18 22:09:48 +0900

    feat: 授業ファクトリにthird_courseを追加

--- spec/factories/courses.rb
+++ spec/factories/courses.rb
@@ -17,5 +17,13 @@ FactoryBot.define do
       day_of_week { '金曜日' }
       course_time { '1限' }
     end
+
+    factory :third_course do
+      association :created_by, factory: :second_teacher
+      name { 'アルゴリズムとデータ構造' }
+      teacher_name { '鈴木一郎' }
+      day_of_week { '月曜日' }
+      course_time { '5限' }
+    end
   end
 end
