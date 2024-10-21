b9173092362cde2aad96666f903451c81ae9f7fc
yuki0829 <fledgling0829@gmail.com>
2024-10-21 14:35:38 +0900

    feat: 授業参加ファクトリにsecond_user_courseを追加

--- spec/factories/user_courses.rb
+++ spec/factories/user_courses.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     association :course
     created_at { Time.zone.now }
     updated_at { Time.zone.now }
+
+    factory :second_user_course do
+      association :course, factory: :second_course
+    end
   end
 end
