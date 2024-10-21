64cf5bd7007d2a22b4eb654b70768c7aa272b578
yuki0829 <fledgling0829@gmail.com>
2024-10-21 14:33:28 +0900

    feat: 授業日ファクトリを作成

---
+++ spec/factories/course_dates.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :course_date do
+    association :course
+    course_number { '第1回' }
+    course_date { '2024/4/1' }
+    is_reflection { true }
+    created_at { Time.zone.now }
+    updated_at { Time.zone.now }
+
+    factory :second_course_date do
+      course_number { '第2回' }
+      course_date { '2024/4/8' }
+    end
+  end
+end
