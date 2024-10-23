8f10c3a82e6e7d4d3977f2439bf3d3ecddba36db
yuki0829 <fledgling0829@gmail.com>
2024-10-23 15:32:55 +0900

    feat: 学校データの取得処理を修正し、廃校の学校を除外、ページング対応を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,28 @@ header = { Authorization: "Bearer #{ENV['SCHOOL_CODE_API_KEY']}", Accept: "appli
 response = HTTParty.get(url, headers: header)
 data = JSON.parse(response.body)
 school_data = data['schools']['data']
+last_page = data['schools']['last_page']
 
-school_data.each do |item|
+school_data.each do |school|
+  next if school['school_status'] == '廃校'
+
+  model_params = {
+    name: school['school_name'],
+  }
+  School.create!(model_params)
+end
+
+(2..last_page).each do |page|
+  response = HTTParty.get("#{url}&page=#{page}", headers: header)
+  data = JSON.parse(response.body)
+  school_data = data['schools']['data']
+
+  school_data.each do |school|
+    next if school['school_status'] == '廃校'
+    
     model_params = {
-        name: item['school_name'],
-      }
+      name: school['school_name'],
+    }
     School.create!(model_params)
+  end
 end
